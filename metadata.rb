61da88c8ec9407c38b3bc2dbfb9fd67af6a975a1
willejs <williamejsalt@gmail.com>
2014-07-31 15:47:21 -0400

    changed to version 1.0.1 to match new tag

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'will@crowdsurge.com'
 license          'Apache 2.0'
 description      'Installs/Configures godoc'
 long_description 'Installs/Configures godoc'
-version          '0.1.1'
+version          '1.0.1'
 
 depends 'golang'
 depends 'nginx'
