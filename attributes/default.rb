edc11f0a5bd8254667951226ad7226930b961b0d
Igor Shpakov <igorshp@gmail.com>
2014-09-09 11:51:51 +0100

    added gopath and gobin env variables to upstart

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ default["godoc"]["user"] = "godoc"
 default["godoc"]["home"] = "#{node['go']['gopath']}/godoc"
 default["godoc"]["nginx"]["proxy"] = false
 default["godoc"]["nginx"]["site"] = "godoc.#{node['domain']}"
+
